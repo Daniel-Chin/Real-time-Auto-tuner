fc8ee5936796957b8f6b7838b69e59323a777ac2
Daniel Chin <daniel_chin@yahoo.com>
2021-01-10 11:35:11 +0800

    it's working! problem: high noise due to concat

--- real-time_auto_tuner.py
+++ real-time_auto_tuner.py
@@ -6,9 +6,11 @@ import numpy as np
 from resampy import resample
 
 SR = 44100
-FRAME_LEN = 2048
+FRAME_LEN = 1024
 DTYPE = (np.float32, pyaudio.paFloat32)
+# FILTER = 'kaiser_fast'
 FILTER = 'kaiser_best'
+FRAME_TIME = 1 / SR * FRAME_LEN
 
 def main():
     pa = pyaudio.PyAudio()
@@ -47,7 +49,7 @@ def autotune(stream, streamOut):
         f0_time = time() - time_start
 
         time_start = time()
-        frame, pitch_to_bend = pitchBend(frame, f0)
+        frame, pitch_to_bend = pitchBend(frame, pitch)
         bender_time = time() - time_start
 
         time_start = time()
@@ -57,7 +59,7 @@ def autotune(stream, streamOut):
         time_start = time()
         display(
             idle_time, f0_time, bender_time, write_time,
-            display_time, pitch_to_bend,
+            display_time, pitch_to_bend, waste
         )
         display_time = time() - time_start
 
@@ -70,9 +72,9 @@ def getLatestFrame(stream):
     ), waste
 
 def pitchBend(frame, its_pitch):
-    pitch_to_bend = - its_pitch % 3
-    if pitch_to_bend < -1.5:
-        pitch_to_bend += 3
+    pitch_to_bend = - (its_pitch % 1)
+    if pitch_to_bend < -.5:
+        pitch_to_bend += 1
     if pitch_to_bend == 0:
         return frame, 0
     freq_oitar = np.exp(- pitch_to_bend * 0.057762265046662105)
@@ -83,18 +85,31 @@ def pitchBend(frame, its_pitch):
         return np.append(frame, frame[frame.size - FRAME_LEN:]), pitch_to_bend
     else:
         # flatter
-        frame = frame[:FRAME_LEN // freq_oitar]
+        frame = frame[:int(FRAME_LEN / freq_oitar)]
         frame = resample(frame, SR, SR * freq_oitar, filter=FILTER)
         if frame.size < FRAME_LEN:
             return np.append(frame, frame[frame.size - FRAME_LEN:]), pitch_to_bend
         else:
             return frame[:FRAME_LEN], pitch_to_bend
 
+METER_WIDTH = 50
+METER = '[' + ' ' * METER_WIDTH + '|' + ' ' * METER_WIDTH + ']'
+METER_CENTER = METER_WIDTH + 1
+TIMES = [
+    'idle_time', 'f0_time', 'bender_time', 'write_time', 
+    'display_time', 
+]
 def display(
     idle_time, f0_time, bender_time, write_time, 
-    display_time, pitch_to_bend, 
+    display_time, pitch_to_bend, waste, 
 ):
-    print(pitch_to_bend)
+    buffer_0 = [*METER]
+    offset = - round(METER_WIDTH * pitch_to_bend * 1.95)
+    buffer_0[METER_CENTER + offset] = '#'
+    _locals = locals()
+    print(*buffer_0, sep='')
+    print(*[x + ' {:4.0%}.   '.format(_locals[x] / FRAME_TIME) for x in TIMES], end='')
+    print('Waste:', max(0, waste))  # samples dumped
 
 def relay(stream, streamOut):
     while True:
